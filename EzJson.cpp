bb7542ee5366fa7a2b0d265da10c34509443cec0
Helei <heleifz@126.com>
2014-04-22 02:15:35 +0800

    refactor done
    TODO : improve performance

--- EzJson.cpp
+++ EzJson.cpp
@@ -1,24 +1,16 @@
-#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
 
-enum TokenType
-{
-	EOS = 0,
-	LCU = '{',
-	RCU = '}',
-	LBR = '[',
-	RBR = ']',
-	COM = ',',
-	COL = ':',
-	NUM,
-	STR,
-	TRU,
-	FAL,
-	NUL,
-	CMT
-};
+#include "EzJson.h"
 
 class ScanError {};
 class ParseError {};
+class NotAnObjectError {};
+class NotAnArrayError {};
+class NotConvertibleError {};
+
 
 class Scanner
 {
@@ -148,8 +140,9 @@ public:
 			case LINECOMMENT:
 				if (current == '\n')
 				{
-					type = CMT;
-					return;
+					// skip comment
+					tokenBegin = tokenEnd;
+					state = START;
 				}
 				break;
 			case BLOCKCOMMENT:
@@ -161,8 +154,9 @@ public:
 			case STAR:
 				if (current == '/')
 				{
-					type = CMT;
-					return;
+					// skip comment
+					tokenBegin = tokenEnd;
+					state = START;
 				}
 				else if (current != '*')
 				{
@@ -180,24 +174,28 @@ public:
 	{
 		return std::string(tokenBegin, tokenEnd);
 	}
-	bool match(TokenType t)
+	void match(TokenType t)
 	{
 		if (t == type)
 		{
 			next();
-			return true;
 		}
-		return false;
+		else
+		{
+			throw ParseError();
+		}
 	}
-	bool match(TokenType t, std::string& out)
+	void match(TokenType t, std::string& out)
 	{
 		if (t == type)
 		{
 			out = std::string(tokenBegin, tokenEnd);
 			next();
-			return true;
 		}
-		return false;
+		else
+		{
+			throw ParseError();
+		}
 	}
 private:
 	enum DFAState
@@ -221,4 +219,461 @@ private:
 	const char* tokenEnd;
 };
 
-// TODO : Parser
+class NumberNode;
+class StringNode;
+class TrueNode;
+class FalseNode;
+class ArrayNode;
+class ObjectNode;
+
+class Visitor : public std::enable_shared_from_this<Visitor>
+{
+public:
+	virtual void visit(NumberNode* ptr) = 0;
+	virtual void visit(TrueNode* ptr) = 0;
+	virtual void visit(FalseNode* ptr) = 0;
+	virtual void visit(StringNode* ptr) = 0;
+	virtual void visit(ArrayNode* ptr) = 0;
+	virtual void visit(ObjectNode* ptr) = 0;
+};
+
+class Node
+{
+public:
+	virtual ~Node() { }
+	virtual void acceptVisitor(std::shared_ptr<Visitor> visitor) = 0;
+	virtual std::shared_ptr<Node> field(const std::string& key)
+	{
+		throw NotAnObjectError();
+	}
+	virtual std::shared_ptr<Node> at(int idx)
+	{
+		throw NotAnArrayError();
+	}
+	virtual double asDouble()
+	{
+		throw NotConvertibleError();
+	}
+	virtual std::string asString()
+	{
+		throw NotConvertibleError();
+	}
+	virtual bool asBool()
+	{
+		throw NotConvertibleError();
+	}
+	virtual std::vector<std::shared_ptr<Node>>& asVectorRef() {
+		throw NotConvertibleError();
+	}
+	virtual std::map<std::string, std::shared_ptr<Node>>& asMapRef() {
+		throw NotConvertibleError();
+	}
+};
+
+class NumberNode : public Node
+{
+public:
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	explicit NumberNode(const std::string& val) : value(val) {}
+	virtual double asDouble()
+	{
+		return std::stod(value);
+	}
+	virtual std::string asString()
+	{
+		return value;
+	}
+private:
+	std::string value;
+};
+
+// string
+class StringNode : public Node
+{
+public:
+	explicit StringNode(const std::string& val) : value(val) {}
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	virtual std::string asString()
+	{
+		return value;
+	}
+private:
+	std::string value;
+};
+
+// true / false
+class TrueNode : public Node
+{
+public:
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	virtual double asDouble()
+	{
+		return 1.0;
+	}
+	virtual std::string asString()
+	{
+		return "true";
+	}
+	virtual bool asBool()
+	{
+		return true;
+	}
+};
+
+class FalseNode : public Node
+{
+public:
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	virtual double asDouble()
+	{
+		return 0.0;
+	}
+	virtual std::string asString()
+	{
+		return "false";
+	}
+	virtual bool asBool()
+	{
+		return false;
+	}
+};
+
+class ArrayNode : public Node
+{
+public:
+	ArrayNode() {}
+	virtual std::shared_ptr<Node> at(int idx)
+	{
+		return childs[idx];
+	}
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	virtual std::vector<std::shared_ptr<Node>>& asVectorRef() {
+		return childs;
+	}
+private:
+	std::vector<std::shared_ptr<Node>> childs;
+};
+
+class ObjectNode : public Node
+{
+public:
+	virtual std::shared_ptr<Node> field(const std::string& key)
+	{
+		return pairs.at(key);
+	}
+	void acceptVisitor(std::shared_ptr<Visitor> visitor)
+	{
+		visitor->visit(this);
+	}
+	virtual std::map<std::string, std::shared_ptr<Node>>& asMapRef() {
+		return pairs;
+	}
+private:
+	std::map<std::string, std::shared_ptr<Node>> pairs;
+};
+
+class Parser {
+public:
+	Parser(const Scanner& sc)
+		: scanner(sc)
+	{ }
+	std::shared_ptr<Node> parseNumber()
+	{
+		std::string val;
+		scanner.match(NUM, val);
+		return std::make_shared<NumberNode>(val);
+	}
+	std::shared_ptr<Node> parseTrue()
+	{
+		scanner.match(TRU);
+		return std::make_shared<TrueNode>();
+	}
+	std::shared_ptr<Node> parseFalse()
+	{
+		scanner.match(FAL);
+		return std::make_shared<FalseNode>();
+	}
+	std::shared_ptr<Node> parseNull()
+	{
+		scanner.match(NUL);
+		return nullptr;
+	}
+	std::shared_ptr<Node> parseString()
+	{
+		std::string val;
+		scanner.match(STR, val);
+		return std::make_shared<StringNode>(std::string(val.begin() + 1, val.end() - 1));
+	}
+	std::shared_ptr<Node> parseValue()
+	{
+		TokenType tk = scanner.lookahead();
+		std::string val;
+		switch (tk)
+		{
+		case NUM:
+			return parseNumber();
+		case STR:
+			return parseString();
+		case LCU:
+			return parseObject();
+		case LBR:
+			return parseArray();
+		case FAL:
+			return parseFalse();
+		case TRU:
+			return parseTrue();
+		case NUL:
+			return parseNull();
+		default:
+			throw ParseError();
+		}
+	}
+	std::shared_ptr<Node> parseArray()
+	{
+		std::shared_ptr<ArrayNode> ret = std::make_shared<ArrayNode>();
+		scanner.match(LBR);
+		if (scanner.lookahead() == RBR)
+		{
+			scanner.next();
+			return ret;
+		}
+		ret->asVectorRef().push_back(parseValue());
+		while (scanner.lookahead() == ',')
+		{
+			scanner.next();
+			ret->asVectorRef().push_back(parseValue());
+		}
+		scanner.match(RBR);
+		return ret;
+	}
+	std::shared_ptr<Node> parseObject()
+	{
+		std::shared_ptr<ObjectNode> ret = std::make_shared<ObjectNode>();
+		scanner.match(LCU);
+		if (scanner.lookahead() == RCU)
+		{
+			scanner.next();
+			return ret;
+		}
+		std::string key;
+		std::shared_ptr<Node> val;
+		scanner.match(STR, key);
+		scanner.match(COL);
+		val = parseValue();
+		ret->asMapRef().insert(std::make_pair(std::string(key.begin() + 1, key.end() - 1), val));
+		while (scanner.lookahead() == COM)
+		{
+			scanner.next();
+			scanner.match(STR, key);
+			scanner.match(COL);
+			val = parseValue();
+			ret->asMapRef().insert(std::make_pair(std::string(key.begin() + 1, key.end() - 1), val));
+		}
+		scanner.match(RCU);
+		return ret;
+	}
+private:
+	Scanner scanner;
+};
+
+template <class STREAM>
+class PrintVisitor : public Visitor
+{
+private:
+	int indent;
+	STREAM& stream;
+public:
+	// initial indention is 0
+	PrintVisitor(STREAM& s) : stream(s), indent(0) {}
+
+	void visit(NumberNode* ptr)
+	{
+		stream << ptr->asString();
+	}
+	void visit(TrueNode* ptr)
+	{
+		stream << ptr->asString();
+	}
+	void visit(FalseNode* ptr)
+	{
+		stream << ptr->asString();
+	}
+	void visit(StringNode* ptr)
+	{
+		stream << '"' << ptr->asString() << '"';
+	}
+	void visit(ArrayNode* ptr)
+	{
+		stream << "[";
+		auto& vec = ptr->asVectorRef();
+		for (int i = 0; i != vec.size(); ++i)
+		{
+			if (vec[i])
+			{
+				vec[i]->acceptVisitor(shared_from_this());
+			}
+			else
+			{
+				stream << "null";
+			}
+			if (i != vec.size() - 1)
+			{
+				stream << ", ";
+			}
+		}
+		stream << "]";
+	}
+	void visit(ObjectNode* ptr)
+	{
+		stream << (indent == 0 ? "" : "\n") << std::string(indent, ' ') << "{" << std::endl;
+		indent += 4;
+
+		auto& mp = ptr->asMapRef();
+
+		if (!mp.empty())
+		{
+			auto i = mp.begin();
+			stream << std::string(indent, ' ') << '"' <<
+				i->first << '"' << " : ";
+			if (i->second)
+			{
+				i->second->acceptVisitor(shared_from_this());
+			}
+			i++;
+			for (; i != mp.end(); ++i)
+			{
+				stream << ",\n";
+				stream << std::string(indent, ' ') << '"' << i->first << '"' << " : ";
+				if (i->second)
+				{
+					i->second->acceptVisitor(shared_from_this());
+				}
+				else
+				{
+					stream << "null";
+				}
+			}
+		}
+		indent -= 4;
+		stream << std::endl << std::string(indent, ' ') << "}";
+	}
+};
+
+JSON JSON::fromNumber(double num) {
+	std::stringstream ss;
+	ss << num;
+	auto n = std::make_shared<NumberNode>(ss.str());
+	return JSON(n);
+}
+JSON JSON::fromBool(bool b)
+{
+	if (b)
+	{
+		return JSON(std::make_shared<TrueNode>());
+	}
+	else
+	{
+		return JSON(std::make_shared<FalseNode>());
+	}
+}
+JSON JSON::fromString(const std::string& str)
+{
+	return JSON(std::make_shared<StringNode>(str));
+}
+JSON JSON::makeObject()
+{
+	return JSON(std::make_shared<ObjectNode>());
+}
+JSON JSON::makeArray()
+{
+	return JSON(std::make_shared<ArrayNode>());
+}
+// construct from file
+JSON::JSON(const char *path) {
+	FILE *f = fopen(path, "rb");
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	auto content =  new char[fsize + 1];
+	fread(content, fsize, 1, f);
+	fclose(f);
+	content[fsize] = '\0';
+	try
+	{
+		Parser p = Parser(Scanner(content));
+		node = p.parseValue();
+		delete[] content;
+	}
+	catch (...)
+	{
+		delete[] content;
+		throw;
+	}
+}
+JSON JSON::field(const std::string& key) const
+{
+	return JSON(node->field(key));
+}
+JSON JSON::at(int idx) const
+{
+	return JSON(node->at(idx));
+}
+double JSON::asDouble() const
+{
+	return node->asDouble();
+}
+std::string JSON::asString() const
+{
+	std::stringstream ss;
+	auto v = std::make_shared<PrintVisitor<std::stringstream>>(ss);
+	node->acceptVisitor(v);
+	return ss.str();
+}
+bool JSON::asBool() const
+{
+	return node->asBool();
+}
+JSON& JSON::append(const JSON& child)
+{
+	node->asVectorRef().push_back(child.node);
+	return *this;
+}
+JSON& JSON::set(int idx, const JSON& child)
+{
+	node->asVectorRef()[idx] = child.node;
+	return *this;
+}
+JSON& JSON::set(const std::string& field, const JSON& value)
+{
+	node->asMapRef()[field] = value.node;
+	return *this;
+}
+std::vector<std::string> JSON::allFields() const
+{
+	auto& mp = node->asMapRef();
+	std::vector<std::string> result;
+	for (auto i = mp.begin(); i != mp.end(); ++i)
+	{
+		result.push_back(i->first);
+	}
+	return result;
+}
+int JSON::arraySize() {
+	return node->asVectorRef().size();
+}
--- main.cpp
+++ main.cpp
@@ -13,44 +13,45 @@
 
 int main()
 {
-	int position = 0;
-	std::ifstream is("test.txt");
-	auto in = std::istreambuf_iterator<char>(is);
-	std::string s(in, std::istreambuf_iterator<char>());;
-	const char* f = s.c_str();
-	Scanner scan(f);
-	while (1)
-	{
-		if (scan.lookahead() == TokenType::EOS)
-			break;
-		std::cout << scan.lookahead() << " && " << scan.text() << std::endl;
-		scan.next();
-	}
+	//int position = 0;
+	//std::ifstream is("test.txt");
+	//auto in = std::istreambuf_iterator<char>(is);
+	//std::string s(in, std::istreambuf_iterator<char>());;
+	//const char* f = s.c_str();
+	//Scanner scan(f);
+	//while (1)
+	//{
+	//	if (scan.lookahead() == TokenType::EOS)
+	//		break;
+	//	std::cout << scan.lookahead() << " && " << scan.text() << std::endl;
+	//	scan.next();
+	//}
 
 	// detect memory leak
 	{
-	/*	JSON j("test5.txt");
+		clock_t t = clock();
+		JSON j("test5.txt");
+		std::cout << clock() - t;
+		/*auto db = j.field("instruments").
+			at(0).
+			field("default_pan").
+			asDouble();
 
-		auto db = j.field("instruments").
-					at(0).
-					field("default_pan").
-					asDouble();
+			std::cout << db;
 
-		std::cout << db;
-
-		JSON other = JSON::makeObject().
+			JSON other = JSON::makeObject().
 			set("hello", JSON::fromNumber(3)).
 			set("world", JSON::fromBool(true)).
 			set("hello", JSON::fromString("nihao")).
 			set("f", JSON::fromString("F word"));
 
-		JSON another = JSON::makeArray().append(other).append(JSON::fromNumber(4.3));
+			JSON another = JSON::makeArray().append(other).append(JSON::fromNumber(4.3));
 
-		std::cout << another;
-		
-		std::ofstream ofs("testout.txt");
-		ofs << another;
-*/
+			std::cout << another;
+
+			std::ofstream ofs("testout.txt");
+			ofs << another;*/
+
 	}
 	_CrtDumpMemoryLeaks();
 	return 0;
